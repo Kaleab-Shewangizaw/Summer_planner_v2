1b2a1482c0b42759aa0c235615b414601465e764
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-07-17 21:28:29 +0300

    started the preview section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Features2 from "@/landing page compoenents/Features2";
 import FeaturesSection from "@/landing page compoenents/FeaturesSection";
 import HeroSection from "@/landing page compoenents/HeroSection";
 import LandingNavbar from "@/landing page compoenents/Navbar";
+import PreviewSection from "@/landing page compoenents/PreviewSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <ChosenBySection />
       <FeaturesSection />
       <Features2 />
+      <PreviewSection />
       <div className=" w-0  rounded-full shadow-[0px_0px_300px_100px_#1447e6] absolute top-70 right-50 "></div>
       <div className=" w-0  rounded-full shadow-[0px_0px_150px_50px_#1447e6] absolute top-50 left-20 "></div>
       <div className=" w-0  rounded-full shadow-[0px_0px_200px_80px_#1447e6] absolute top-180 left-100 z-[-1]"></div>
---
+++ src/landing page compoenents/PreviewSection.tsx
@@ -0,0 +1,3 @@
+export default function PreviewSection() {
+  return <div>this will be the preview</div>;
+}
