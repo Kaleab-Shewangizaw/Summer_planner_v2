6a219654a75b9af7b04f5d8399fd855a027e1a87
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-07-19 05:22:35 +0300

    exit animation for accordion componenet

--- src/landing page compoenents/Accordion.tsx
+++ src/landing page compoenents/Accordion.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { easeIn, motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { BiMinus, BiPlus } from "react-icons/bi";
 
 export default function Accordion({
@@ -12,29 +12,30 @@ export default function Accordion({
   content: string;
 }) {
   const [expand, setExpand] = useState<boolean>(false);
+
   return (
     <div className="w-[80%] bg-[#181f2e] rounded-lg">
       <div
         className="flex cursor-pointer items-center font-bold w-full justify-between p-3 mb-0"
-        onClick={() => {
-          setExpand(!expand);
-        }}
+        onClick={() => setExpand(!expand)}
       >
         {title} {expand ? <BiMinus /> : <BiPlus />}
       </div>
-      {expand && (
-        <motion.div
-          key="content"
-          initial={{ height: 0, opacity: 0 }}
-          animate={{ height: "auto", opacity: 1 }}
-          // TODO
-          exit={{ height: 0, opacity: 0 }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-          className="overflow-hidden px-3 mt-0 pb-3"
-        >
-          {content}
-        </motion.div>
-      )}
+
+      <AnimatePresence initial={false}>
+        {expand && (
+          <motion.div
+            key="content"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="overflow-hidden px-3 mt-0 pb-3"
+          >
+            {content}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
