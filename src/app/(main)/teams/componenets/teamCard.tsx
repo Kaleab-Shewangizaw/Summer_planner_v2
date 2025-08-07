f9af5213f5782467efb21698f1bf57b514b27f4c
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-08-07 20:06:10 +0300

    team card edit

--- src/app/(main)/teams/componenets/teamCard.tsx
+++ src/app/(main)/teams/componenets/teamCard.tsx
@@ -1,7 +1,7 @@
 export default function TeamsCard() {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-sm border w-40">
-      <h1 className="text-2xl font-bold">Team Card Component</h1>
+    <div className="flex flex-col items-center justify-center h-full text-sm border w-60 border-gray-600 px-2 py-1 rounded-md">
+      <h1 className="text-lg font-bold">Team Card Component</h1>
       <p className="text-gray-500">This is a placeholder for the team card.</p>
     </div>
   );
